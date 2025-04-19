88a7f2d1d94b563aacec21dbd0aadf6507fb87bb
Tushar Shaw <128092800+STusharADS@users.noreply.github.com>
2025-04-20 01:39:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='underline text-red-400'>ECommerce</div>
+    </>
+  )
+}
+
+export default App
